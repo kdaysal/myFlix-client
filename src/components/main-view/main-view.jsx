19adfa2ceb38e0d1e4b4c5787cca49a092db033f
Kevin Daysal <kdaysal123@gmail.com>
2022-04-21 22:37:25 -0400

    Imported MovieCard component and added prop: 'movieData={movie}'

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MovieCard } from './movie-card/movie-card';
 
 export default class MainView extends React.Component { //by adding 'default', I won't need to enclose 'MainView' in {curly braces} for import statements
 
@@ -20,26 +21,34 @@ export default class MainView extends React.Component { //by adding 'default', I
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
     //code below will only run if [movies] is not empty. Syntax note: 'Else' logic is implied because if the 'return' statement above were called, the function would stop executing before this line
 
-    //added 'key={movie_id}' to the movie title <div> in order to resolve the console warning: Each child in a list should have a unique "key" prop.
+    //<MovieCard /> component replaces: <div key={movie._id}>{movie.Title}</div>. But still must add 'key={movie._id}'
+    //'movieData={movie}' is a prop - an attribute that can pass data to a child component
     return (
       <div className="main-view">
-        {movies.map(movie => <div key={movie._id}>{movie.Title}</div>)}
+        {movies.map(movie => <MovieCard key={movie._id} movieData={movie} />)}
       </div>
     );
   }
-
-  render() {
-    const movies = this.state.movies;
-    if (movies.length === 0) {
-      return <div className="main-view">The list is empty!</div>;
-    } else {
-      return (
-        <div className="main-view">
-          {movies.map((movie) => {
-            return <div key={movie._id}>{movie.Title}</div>; //added 'key={movie_id}' to <div> to resolve console warning: Each child in a list should have a unique "key" prop.
-          })}
-        </div>
-      );
-    }
-  }
 }
+
+/* *************************************** */
+//ORIGINAL CODE BLOCK for render() function
+
+//   render() {
+//     const movies = this.state.movies;
+//     if (movies.length === 0) {
+//       return <div className="main-view">The list is empty!</div>;
+//     } else {
+//       return (
+          //added 'key={movie_id}' to the movie title <div> in order to resolve the console warning: Each child in a list should have a unique "key" prop.
+//         <div className="main-view">
+//           {movies.map((movie) => {
+//             return <div key={movie._id}>{movie.Title}</div>; //added 'key={movie_id}' to <div> to resolve console warning: Each child in a list should have a unique "key" prop.
+//           })}
+//         </div>
+//       );
+//     }
+//   }
+// }
+
+/* *************************************** */
