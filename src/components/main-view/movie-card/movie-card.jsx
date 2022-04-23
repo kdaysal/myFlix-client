abf2830f619bede7605ff1016ed977a27d25ddcd
Kevin Daysal <kdaysal123@gmail.com>
2022-04-22 21:21:32 -0400

    Added comment to note the different syntax in this export statement
    No 'default' compared to the export statement in main-view.jsx ('export default...') - just for learning purposes

--- src/components/main-view/movie-card/movie-card.jsx
+++ src/components/main-view/movie-card/movie-card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 //create a MovieCard component
 //having granular components like this makes it easier to reuse these smaller components in different parts of the UI
-export class MovieCard extends React.Component {
+export class MovieCard extends React.Component { //(just for sake of example) here there is no 'default' after 'export', so I'll need to enclose 'MovieCard' in {curly braces} in any import statement.
     render() {
         const { movieData } = this.props; //accessing (extracting) the passed data via 'props' property ('movieData' is the name of the prop used in <MovieCard ... />)
         return <div className="movie-card" >{movieData.Title}</div>;
