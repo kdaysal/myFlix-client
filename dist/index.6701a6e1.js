ba952e4c86565cc62559b5db80322ad6cbe42319
Kevin Daysal <kdaysal123@gmail.com>
2022-04-23 00:43:59 -0400

    Updated (hardcoded) movie array data with data from my db (copy/pasted for now)

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -25226,27 +25226,27 @@ class MainView extends _reactDefault.default.Component {
             movies: [
                 {
                     _id: 1,
-                    Title: 'Inception',
-                    Description: 'desc1...',
-                    ImagePath: '...',
-                    Genre: 'genre1',
-                    Director: 'director1'
+                    Title: 'Sausage Party',
+                    Description: 'Sausage Party is a 2016 adult computer-animated comedy film directed by Conrad Vernon and Greg Tiernan and written by Kyle Hunter, Ariel Shaffir, Seth Rogen, and Evan Goldberg from a story by Rogen, Goldberg, and Jonah Hill. A parody of Disney and Pixar films, the film follows an anthropomorphic sausage who lives in a supermarket and discovers the truth about what happens when groceries are purchased',
+                    ImagePath: 'https://www.imdb.com/title/tt1700841/mediaviewer/rm948965376/',
+                    Genre: 'Animated',
+                    Director: 'Conrad Vernon'
                 },
                 {
                     _id: 2,
-                    Title: 'The Shawshank Redemption',
-                    Description: 'desc2...',
-                    ImagePath: '...',
-                    Genre: 'genre2',
-                    Director: 'director2'
+                    Title: 'The Village',
+                    Description: 'The Village is a 2004 American period thriller film written, produced, and directed by M. Night Shyamalan. It stars Bryce Dallas Howard, Joaquin Phoenix, Adrien Brody, William Hurt, Sigourney Weaver, and Brendan Gleeson. The film is about a village whose population lives in fear of creatures inhabiting the woods beyond it, referred to as Those of Whom We Do Not Speak',
+                    ImagePath: 'https://www.imdb.com/title/tt0368447/mediaviewer/rm2834906624/',
+                    Genre: 'Horror',
+                    Director: 'M. Night Shyamalan'
                 },
                 {
                     _id: 3,
-                    Title: 'Gladiator',
-                    Description: 'desc3...',
-                    ImagePath: '...',
-                    Genre: 'genre3',
-                    Director: 'director3'
+                    Title: 'Pulp Fiction',
+                    Description: 'Pulp Fiction is a 1994 American black comedy crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary. Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles. The title refers to the pulp magazines and hardboiled crime novels popular during the mid-20th century, known for their graphic violence and punchy dialogue',
+                    ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/',
+                    Genre: 'Thriller',
+                    Director: 'Quentin Tarantino'
                 }
             ],
             selectedMovie: null //this variable will represent whether a movie card is clicked (null if no)
