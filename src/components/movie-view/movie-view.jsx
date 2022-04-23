dc3eaa4c301ba1122019da9b352dd448166bedff
Kevin Daysal <kdaysal123@gmail.com>
2022-04-22 22:27:04 -0400

    Correction to variable name 'movie' in render() function

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export class MovieView extends React.Component {
     render() {
-        const { movieObj } = this.props;
+        const { movie } = this.props;
         return (
             <div className="movie-view">
                 <div className="movie-poster">
