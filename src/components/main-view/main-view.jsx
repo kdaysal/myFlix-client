a1ac22b46df2bdceab68966c4a568072cedb00fc
Kevin Daysal <kdaysal123@gmail.com>
2022-04-24 22:05:36 -0400

    Imported axios for ajax operations, and added class component lifecycle method: componentDidMount()
    The idea is to use Axios to fetch the movies, then set the 'state' of movies using this.setState.
    
    Also removed hard-coded movie data ('movies' is now a blank array)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'; //this will allow me to perform ajax operations. Axios will fetch the movies, then I'll set the 'state' of movies using this.setState
 
 //import components
 import { MovieCard } from '../movie-card/movie-card';
@@ -9,13 +10,22 @@ export default class MainView extends React.Component { //by adding 'default', I
   constructor() { //React will use this method to create the component. Always initialize a state's values in the constructor, as it is called before render().
     super(); //call the constructor of the parent class ('React.Component'). This initializes the component's state, and is needed in order for 'this.state' (below) to work.
     this.state = { //initialize state
-      movies: [
-        { _id: 1, Title: 'Sausage Party', Description: 'Sausage Party is a 2016 adult computer-animated comedy film directed by Conrad Vernon and Greg Tiernan and written by Kyle Hunter, Ariel Shaffir, Seth Rogen, and Evan Goldberg from a story by Rogen, Goldberg, and Jonah Hill. A parody of Disney and Pixar films, the film follows an anthropomorphic sausage who lives in a supermarket and discovers the truth about what happens when groceries are purchased', ImagePath: 'https://www.imdb.com/title/tt1700841/mediaviewer/rm948965376/', Genre: 'Animated', Director: 'Conrad Vernon' },
-        { _id: 2, Title: 'The Village', Description: 'The Village is a 2004 American period thriller film written, produced, and directed by M. Night Shyamalan. It stars Bryce Dallas Howard, Joaquin Phoenix, Adrien Brody, William Hurt, Sigourney Weaver, and Brendan Gleeson. The film is about a village whose population lives in fear of creatures inhabiting the woods beyond it, referred to as Those of Whom We Do Not Speak', ImagePath: 'https://www.imdb.com/title/tt0368447/mediaviewer/rm2834906624/', Genre: 'Horror', Director: 'M. Night Shyamalan' },
-        { _id: 3, Title: 'Pulp Fiction', Description: 'Pulp Fiction is a 1994 American black comedy crime film written and directed by Quentin Tarantino, who conceived it with Roger Avary. Starring John Travolta, Samuel L. Jackson, Bruce Willis, Tim Roth, Ving Rhames, and Uma Thurman, it tells several stories of criminal Los Angeles. The title refers to the pulp magazines and hardboiled crime novels popular during the mid-20th century, known for their graphic violence and punchy dialogue', ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/', Genre: 'Thriller', Director: 'Quentin Tarantino' }
-      ],
+      movies: [],
       selectedMovie: null //this variable will represent whether a movie card is clicked (null if no)
     }
+  }
+
+  //this code will execute right after the component is mounted (i.e. right after it is has been fully rendered and added to the DOM)
+  componentDidMount() {
+    axios.get('https://kdaysal-my-flix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   //custom component method 'setselectedMovie'
