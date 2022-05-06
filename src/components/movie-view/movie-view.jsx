e4dea1cd6ccd6d10ea91ba752093a286b06e306f
Kevin Daysal <kdaysal123@gmail.com>
2022-05-05 21:27:25 -0400

    Refactored MovieView return() statement to incorporate React Bootstrap for styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -25,44 +25,28 @@ export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-genre-name">
-          <span className="label">Genre-Name: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-genre-description">
-          <span className="label">Genre-Description: </span>
-          <span className="value">{movie.Genre.Description}</span>
-        </div>
-        <div className="movie-director-name">
-          <span className="label">Director-Name: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-director-bio">
-          <span className="label">Director-Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-        <div className="movie-director-birth">
-          <span className="label">Director-Birth: </span>
-          <span className="value">{movie.Director.Birth}</span>
-        </div>
-        <div className="movie-director-death">
-          <span className="label">Director-Death: </span>
-          <span className="value">{movie.Director.Death}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
+      <Container id="movie-view-container">
+        <Row>
+          <Col>
+            <CardGroup>
+              <Card className="bg-dark text-white">
+                <Card.Body>
+                  <Card.Img className="movie-poster" src={movie.ImagePath} />
+                  <Card.Title className="movie-title">Title: {movie.Title}</Card.Title>
+                  <Card.Text className="movie-description">Description: {movie.Description}</Card.Text>
+                  <Card.Text className="movie-genre-name">Genre: {movie.Genre.Name}</Card.Text>
+                  <Card.Text className="movie-genre-description">Genre Info: {movie.Genre.Description}</Card.Text>
+                  <Card.Text className="movie-director-name">Director: {movie.Director.Name}</Card.Text>
+                  <Card.Text className="movie-director-bio">Bio: {movie.Director.Bio}</Card.Text>
+                  <Card.Text className="movie-director-birth">Birth: {movie.Director.Birth}</Card.Text>
+                  <Card.Text className="movie-director-death">Death: {movie.Director.Death}</Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+            <Button id="back-btn" onClick={() => { onBackClick(null); }}>Back</Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
