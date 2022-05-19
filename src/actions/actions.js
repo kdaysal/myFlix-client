70063c247a27a53064bd5208c18cfae9b80065dd
Kevin Daysal <kdaysal123@gmail.com>
2022-05-19 18:08:40 -0400

    Added comments to explain Action Types and Action Creators

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,19 @@
-export const SET_MOVIES = 'SET_MOVIES'; //SET_MOVIES initializes teh movies list with movies
+//Action Types...
+export const SET_MOVIES = 'SET_MOVIES'; //SET_MOVIES is an 'action type' that initializes the movies list with movies
 export const SET_FILTER = 'SET_FILTER'; //SET_FILTER sets the filter to filter the movies list
 
-//exporting functions is similar to using event constructors - I will call them from a view to formally express the change that I want to perform on the app's state
+//Action Creators...
+//note - exporting functions is similar to using event constructors - I will call them from a view to formally express the change that I want to perform on the app's state
 export function setMovies(value) {
-    return { type: SET_MOVIES, value };
+    return { //this returns an 'action', which is an object
+        type: SET_MOVIES,
+        value
+    };
 }
 
 export function setFilter(value) {
-    return { type: SET_FILTER, value };
+    return {
+        type: SET_FILTER,
+        value
+    };
 }
