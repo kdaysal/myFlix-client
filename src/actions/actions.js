4f8daa37a46fdf8e990a55c3768c590bb5aaee60
Kevin Daysal <kdaysal123@gmail.com>
2022-05-19 17:57:17 -0400

    Defined first two actions in new actions.js file (initial commit of new branch)

---
+++ src/actions/actions.js
@@ -0,0 +1,11 @@
+export const SET_MOVIES = 'SET_MOVIES'; //SET_MOVIES initializes teh movies list with movies
+export const SET_FILTER = 'SET_FILTER'; //SET_FILTER sets the filter to filter the movies list
+
+//exporting functions is similar to using event constructors - I will call them from a view to formally express the change that I want to perform on the app's state
+export function setMovies(value) {
+    return { type: SET_MOVIES, value };
+}
+
+export function setFilter(value) {
+    return { type: SET_FILTER, value };
+}
