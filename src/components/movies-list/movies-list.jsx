f7a21be6db2a05892436a3d20777aaa8e8feb412
Kevin Daysal <kdaysal123@gmail.com>
2022-05-21 13:27:26 -0400

    Added lots of comments for my learning/studying (not for production code)

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -26,4 +26,14 @@ function MoviesList(props) {
     ));
 }
 
-export default connect(mapStateToProps)(MoviesList);
+export default connect(mapStateToProps)(MoviesList); //this connects the MoviesList component to the store, but it only receives the first argument (mapStateToProps).
+
+/* More notes about the 'export default connect...' statement ^ */
+// The first argument, mapStateToProps, is a function that converts or transforms the store into props that the MoviesList component will use.
+// Remember that the store contains my application’s state, which is why this function is called 'mapStateToProps'.
+// The second argument - the list of actions to bind, is implicitly null.
+// This could also be written as: export default connect(mapStateToProps, null)(MoviesList);
+
+// In the mapStateToProps function, I extracted visibilityFilter into a prop named visibilityFilter.
+// This means that MoviesList’s props contain two properties; the second being movies, which was passed when the component was expressed in the render() method of the MainView component.
+// Now, I can filter the array 'movies' based on the value present in visibilityFilter, then render the filtered array into a list of MovieCard components.
