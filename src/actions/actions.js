2fc5706f49ba5a180d7a388411b110c585faa186
Kevin Daysal <kdaysal123@gmail.com>
2022-05-21 21:49:27 -0400

    Created new action 'SET_USER'

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,6 @@
 export const SET_MOVIES = 'SET_MOVIES'; //SET_MOVIES initializes teh movies list with movies
 export const SET_FILTER = 'SET_FILTER'; //SET_FILTER sets the filter to filter the movies list
+export const SET_USER = "SET_USER";
 
 //exporting functions is similar to using event constructors - I will call them from a view to formally express the change that I want to perform on the app's state
 export function setMovies(value) {
@@ -10,4 +11,8 @@ export function setMovies(value) {
 export function setFilter(value) {
     //   console.log('SET_FILTER action triggered');
     return { type: SET_FILTER, value };
+}
+
+export function setUser(value) {
+    return { type: SET_USER, value };
 }
