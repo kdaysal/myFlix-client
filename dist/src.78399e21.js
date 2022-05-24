afd5bb2ba36bc9aa17dd9ed6e36c0371d7d05685
Kevin Daysal <kdaysal123@gmail.com>
2022-05-23 21:44:18 -0400

    Debugging

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -59396,7 +59396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60297" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60548" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
