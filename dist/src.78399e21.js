f16f26ebfbc7098ce4e70e2528b63191db78d822
Kevin Daysal <kdaysal123@gmail.com>
2022-08-08 14:54:36 -0400

    Added image files for use with README.md

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -59396,7 +59396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52834" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55489" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
